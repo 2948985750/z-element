e131b882974bc4df5075499a8cb5b26eaf462434
KZ137onethe <q2274799107@126.com>
2023-03-16 17:28:54 +0800

    fix: :bug: added a simple path to the example in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+import path from 'path'
 // import ancestors from "postcss-nested-ancestors"; 它支持嵌套规则
 // import nested from 'postcss-nested'; //它支持的是类似sass语法的嵌套规则
 import nesting from 'postcss-nesting';
@@ -51,4 +52,10 @@ export default defineConfig({
   // server: {
   //   hmr: { overlay: false },
   // },
+  resolve:{
+    alias:{
+      '@': path.resolve(__dirname, './src'),
+      '~packages': path.resolve(__dirname, './packages/components')
+    }
+  }
 });
